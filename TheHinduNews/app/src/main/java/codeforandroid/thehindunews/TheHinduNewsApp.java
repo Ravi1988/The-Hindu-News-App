c52dff2b0640a87b49b094d4dc3afa19dcfeed56
Ravi Sharma <erravisec@gmail.com>
2018-01-29 01:07:56 +0530

    Initial setup
    Initial setup

---
+++ TheHinduNews/app/src/main/java/codeforandroid/thehindunews/TheHinduNewsApp.java
@@ -0,0 +1,16 @@
+package codeforandroid.thehindunews;
+
+import android.app.Application;
+
+/**
+ * Created by ravi.sharma01 on 1/28/18.
+ */
+
+public class TheHinduNewsApp extends Application{
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+	}
+}
