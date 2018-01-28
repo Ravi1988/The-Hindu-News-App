e8710e6cdad27bf89a853638a6366db831feff8f
Ravi Sharma <erravisec@gmail.com>
2018-01-29 01:33:21 +0530

    creating class structure

---
+++ TheHinduNews/app/src/main/java/codeforandroid/thehindunews/model/NewsItem.java
@@ -0,0 +1,8 @@
+package codeforandroid.thehindunews.model;
+
+/**
+ * Created by ravi.sharma01 on 1/29/18.
+ */
+
+public class NewsItem {
+}
---
+++ TheHinduNews/app/src/main/java/codeforandroid/thehindunews/repo/NewsAPIService.java
@@ -0,0 +1,8 @@
+package codeforandroid.thehindunews.repo;
+
+/**
+ * Created by ravi.sharma01 on 1/29/18.
+ */
+
+public class NewsAPIService {
+}
